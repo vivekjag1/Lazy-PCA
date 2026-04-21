0c311b95fc27b8fc2b92ef99f43182e6d5b4a61e
Vivek Jagadeesh <vivekjag1@gmail.com>
2026-04-21 15:49:02 -0400

    refactor PCA to be a clas

--- power_iteration.py
+++ power_iteration.py
@@ -1,5 +1,4 @@
 import numpy as np # version 2.4.2
-
 # constants 
 NUM_ITERATIONS = 2000
 EPSILON = 1e-6 
@@ -7,52 +6,60 @@ EPSILON = 1e-6
 # this will need to be tuned 
 NUM_COMPONENTS = 2
 
-# power iteration to find a single component. 
-def power_iteration(X):
-    # find M (this is just XTX)
-    M = X.T @ X
-
-    # find random vector V in the shpe of M 
-    rng = np.random.default_rng()
-    v = rng.standard_normal(M.shape[0])
-    # make v a unit vector 
-    v0 = v / np.linalg.norm(v)
-
-    v_prev = None
-
-    for i in range(NUM_ITERATIONS): 
-        # if i = 0, skip squaring 
-        if i>0: 
-            M = M @ M 
-
-        # find new V 
-        v_new = M @ v0
-
-        # normalize V 
-        v_new = v_new / np.linalg.norm(v_new)
-
-        # convergence check 
-        if v_prev is not None and np.linalg.norm(v_new - v_prev) < EPSILON:
-            return v_new 
-        v_prev = v_new  
-    return v_prev
 
 
+class PowerIteration(): 
+    def __init__(self, num_iterations, epsilon, num_components, features): 
+        self.num_iterations = num_iterations
+        self.epsilon = epsilon
+        self.num_components = num_components
+        self.features = features
+
+    # power iteration to find a single component. 
+    def power_iteration_single_iteration(self):
+        # find M (this is just XTX)
+        M = self.features.T @ self.features
+
+        # find random vector V in the shpe of M 
+        rng = np.random.default_rng()
+        v = rng.standard_normal(M.shape[0])
+        # make v a unit vector 
+        v0 = v / np.linalg.norm(v)
+
+        v_prev = None
+
+        for i in range(NUM_ITERATIONS): 
+            # if i = 0, skip squaring 
+            if i>0: 
+                M = M @ M 
+
+            # find new V 
+            v_new = M @ v0
+
+            # normalize V 
+            v_new = v_new / np.linalg.norm(v_new)
+
+            # convergence check 
+            if v_prev is not None and np.linalg.norm(v_new - v_prev) < EPSILON:
+                return v_new 
+            v_prev = v_new  
+        return v_prev
+    
+    def power_iteration(self): 
+        components = []
+        for i in range(self.num_components): 
+            components.append(self.power_iteration_single_iteration())
+            w = components[i]
+            self.features = self.features - np.outer(X @ w, w)
+        return components
 
 
 if __name__ == "__main__": 
     # at some point, we'll have embeddings. right now we dont, so im making a dummy X matrix 
     X = np.random.rand(5,2)
 
-    components = []
-
-    for i in range(NUM_COMPONENTS): 
-        # find each component
-        components.append(power_iteration(X))
-
-        w = components[i]
-        # computer the projection of X onto W, then subtract it from X
-        X = X - np.outer(X @ w, w)
+    power_iter = PowerIteration(NUM_ITERATIONS, EPSILON, NUM_COMPONENTS, X)
+    components = power_iter.power_iteration()
     print(components)
 
     
